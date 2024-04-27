9e2f71aa947ce15d0e50bc9afb7fa3255f41f203
Ridwan <phercerchridwan98@gmail>
2024-04-27 15:44:23 +0100

    added a barcode

--- app/(component)/barcode/page.jsx
+++ app/(component)/barcode/page.jsx
@@ -10,8 +10,8 @@ import React from 'react'
 const page = () => {
   return (
     <div className='mt-24 flex items-center justify-center '>
-     {/* <Barcode /> */}
-    <QrReader />
+     <Barcode />
+    {/* <QrReader /> */}
     </div>
   )
 }
